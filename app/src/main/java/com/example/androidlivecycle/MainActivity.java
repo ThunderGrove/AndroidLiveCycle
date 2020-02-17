09fa133b494b6af87df2d84ad7315b8ad75193cf
ThunderGrove <rene699f@edu.easj.dk>
2020-02-17 10:46:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidlivecycle/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.androidlivecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"App init");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.d(TAG,"App started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG,"App resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG,"App paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG,"App stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG,"App destroyed");
+    }
+}
